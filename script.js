62c94cd2024ea1364da8458e8879380dd118e458
axgu <86838870+axgu@users.noreply.github.com>
2022-06-09 18:37:13 -0400

    Competition info

---
+++ script.js
@@ -0,0 +1,10 @@
+function accordionClick(element){
+    element.classList.toggle("active");
+    var panel = element.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+}
+   
